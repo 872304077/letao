d6aec1ae4ad3153906e32d91d6f9de5fcf5f5d81
Your Name <luowei@qq.com>
2017-11-10 20:45:32 +0800

    一级分类已经完成

---
+++ public/back/js/first.js
@@ -0,0 +1,80 @@
+$(function() {
+
+    var currentPage = 1;
+    var pageSize = 5;
+
+    function render() {
+        $.ajax({
+            type: "get",
+            url: "/category/queryTopCategoryPaging",
+            data: {
+                page: currentPage,
+                pageSize: pageSize,
+            },
+            success: function(data) {
+                console.log(data);
+                var html = template('tpl', data);
+                $('tbody').html(html);
+
+                $("#paginator").bootstrapPaginator({
+                    bootstrapMajorVersion: 3, //指定bootstrap的版本，如果是3，必须指定
+                    currentPage: currentPage, //指定当前页
+                    totalPages: Math.ceil(data.total / pageSize), //指定总页数
+                    onPageClicked: function(a, b, c, page) {
+                        //page指的是点击的页码,修改了当前页
+                        currentPage = page;
+                        //重新渲染
+                        render();
+                    }
+                });
+            }
+        });
+    }
+    render();
+
+    $('.mb_15').on('click', function() {
+        $('#addModal').modal('show');
+    })
+    var $form = $('form');
+    $form.bootstrapValidator({
+        //小图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        //校验规则
+        fields: {
+            categoryName: {
+
+                validators: {
+                    //非空
+                    notEmpty: {
+                        message: "请输入一级分类"
+                    }
+                }
+
+            }
+        }
+    });
+
+    $form.on("success.form.bv", function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            url: "/category/addTopCategory",
+            data: $form.serialize(),
+            success: function(data) {
+                if (data.success) {
+                    $('#addModal').modal('hide');
+                    currentPage = 1;
+                    render();
+
+                    $form.data('bootstrapValidator').resetFrom();
+                    $from[0].reset();
+                }
+            }
+        });
+    })
+
+})
