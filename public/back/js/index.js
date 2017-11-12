1d8d3c12f164cc26d7b4b5dd7ff261058ee7e62b
Your Name <luowei@qq.com>
2017-11-13 00:11:49 +0800

    后台和移动端架子完成

--- public/back/js/index.js
+++ public/back/js/index.js
@@ -1,63 +1,74 @@
-$(function() {
-    var myChart = echarts.init(document.querySelector('.in_left'));
+/**
+ * Created by HUCC on 2017/11/8.
+ */
+$(function () {
 
-    var option = {
-        title: {
-            text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-            data: ['销量']
-        },
-        xAxis: {
-            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
-    };
+  var myChart = echarts.init(document.querySelector(".pic_left"));
 
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
-    var myChart = echarts.init(document.querySelector('.in_right'));
-    option = {
-        title: {
-            text: '某站点用户访问来源',
-            subtext: '纯属虚构',
-            x: 'center'
-        },
-        tooltip: {
-            trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} ({d}%)"
-        },
-        legend: {
-            orient: 'vertical',
-            left: 'left',
-            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-        },
-        series: [{
-            name: '访问来源',
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '60%'],
-            data: [
-                { value: 335, name: '直接访问' },
-                { value: 310, name: '邮件营销' },
-                { value: 234, name: '联盟广告' },
-                { value: 135, name: '视频广告' },
-                { value: 1548, name: '搜索引擎' }
-            ],
-            itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-            }
-        }]
-    };
-    myChart.setOption(option);
+  // 指定图表的配置项和数据
+  var option = {
+    title: {
+      text: '2017年注册人数'
+    },
+    tooltip: {},
+    legend: {
+      data:['人数']
+    },
+    xAxis: {
+      data: ["1月","2月","3月","4月","5月","6月"]
+    },
+    yAxis: {},
+    series: [{
+      name: '人数',
+      type: 'bar',
+      data: [1000, 1499, 800, 2000, 1300, 1600]
+    }]
+  };
+
+  myChart.setOption(option);
+
+
+  var myChart1 = echarts.init(document.querySelector(".pic_right"));
+  var option1 = {
+    title : {
+      text: '热门品牌销售',
+      subtext: '2017年6月',
+      x:'center'
+    },
+    tooltip : {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['阿迪','耐克','李宁','乔丹','安踏']
+    },
+    series : [
+      {
+        name: '销售情况',
+        type: 'pie',
+        radius : '55%',
+        center: ['50%', '60%'],
+        data:[
+          {value:335, name:'阿迪'},
+          {value:310, name:'耐克'},
+          {value:234, name:'李宁'},
+          {value:135, name:'乔丹'},
+          {value:1548, name:'安踏'}
+        ],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+  myChart1.setOption(option1);
+
+
 });
